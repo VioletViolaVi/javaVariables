e6c2e3b9b30047b62c57b9171c6d1558cc8fe01f
VioletViola <vivian.v18723@gmail.com>
2021-08-15 19:25:01 +0100

    Practice with variables in java.

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        String myName = "Raven";
+        System.out.println(myName);
+
+        int myAge;
+        myAge = 29;
+        System.out.println(myAge);
+
+        double myHeight = 19.75;
+        System.out.println(myHeight);
+
+        String myFavFood = "fish & chips";
+        myFavFood = "burgers";
+        System.out.println(myFavFood);
+    }
+}
